0822d1ab9bf1347fd16948be72d7ce8113ab4469
sani salim lawan <sanisalim2005@gmail.com>
2023-12-08 01:26:48 +0100

    first commit

--- app.js
+++ app.js
@@ -9,74 +9,20 @@ const menu = document.querySelector('.menu').addEventListener('click', e => {
 
 const data = [
    {
-     name: 'John Doe',
-     age: 32,
-     gender: 'male',
-     lookingfor: 'female',
-     location: 'Boston MA',
-     image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    text: `<h1> I no longer have to sniff other dog for love. I\' have found the hottest cogi Tin dog. woof,</h1>`,
+    img: '<img src="images/dog-img.jpg" alt="dog profile" class="dog-img">'
    },
-   {
-     name: 'Jen Smith',
-     age: 26,
-     gender: 'female',
-     lookingfor: 'male',
-     location: 'Miami FL',
-     image: 'https://randomuser.me/api/portraits/women/82.jpg'
-   },
-   {
-     name: 'William Johnson',
-     age: 38,
-     gender: 'male',
-     lookingfor: 'female',
-     location: 'Lynn MA',
-     image: 'https://randomuser.me/api/portraits/men/83.jpg'
-   }
+  //  {
+  //    text: 'I no longer have to sniff other dog for love. I\' have found the hottest cogi Tin dog. woof',
+  //    img: `<img src="images/angela.png" alt="dog profile" class="dog-img">`
+  //  },
+  //  {
+  //   text: 'I no longer have to sniff other dog for love. I\' have found the hottest cogi Tin dog. woof',
+  //   img: `<img src="images/HTMAA-thumbnail.png" alt="dog profile" class="dog-img">`
+  //  }
  ];
- 
- const profiles = profileIterator(data);
- 
- // Call first profile
-//  nextProfile();
- 
- // Next Event
-//  document.getElementById('next').addEventListener('click', nextProfile);
- 
- // Next Profile Display
-//  function nextProfile() {
-//    const currentProfile = profiles.next().value;
- 
-//    if(currentProfile !== undefined) {
-//      document.getElementById('profileDisplay').innerHTML = `
-//        <ul class="list-group">
-//          <li class="list-group-item">Name: ${currentProfile.name}</li>
-//          <li class="list-group-item">Age: ${currentProfile.age}</li>
-//          <li class="list-group-item">Location: ${currentProfile.location}</li>
-//          <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-//        </ul>
-//      `;
- 
-//      document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
-//    } else {
-//      // No more profiles
-//      window.location.reload();
-//    }
-//  }
- 
- // Profile Iterator
- function profileIterator(profiles) {
-   let nextIndex = 0;
- 
-   return {
-     next: function() {
-       return nextIndex < profiles.length ? 
-       { value: profiles[nextIndex++], done: false } : 
-       { done: true }
-     }
-   };
-   console.log(next().value);
 
- }
- profileIterator(profiles)
+ document.querySelector('.tes').innerHTML = data.text.value;
+ console.log0(data)
 
  
